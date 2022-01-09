aef75411051d4cb8cc2f629084d45367459ea920
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-09 19:53:03 +0100

    Junit grouped Assertions in testing the Person class at PersonTest.java

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupAssertions() {
+        // given
+        Person person = new Person(1L, "Joe", "Buck");
+
+        // then
+
+        assertAll("Test Props Set",
+                // here comes a list of lambda expressions! as many we need!
+                () -> assertEquals("Joe", person.getFirstName()),
+                () -> assertEquals("Buck", person.getLastName()));
+    }
+
+
+    @Test
+    void groupAssertionsMessages() {
+        // given
+        Person person = new Person(1L, "Joe", "Buck");
+
+        // then
+
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName(), "First Name Failed!"),
+                () -> assertEquals("Buck", person.getLastName(), "Last Name Failed!!!"));
+    }
+}
