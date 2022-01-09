132534ec98a6fd09ae98ba79e857583d9ca06fd2
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-09 20:42:35 +0100

    skipping Junit Tests with using @Disable annotation OwnerSDJpaServiceTest.java

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// @Disabled // can be marked entire class disabled!!! or just a test method see bellow!
+// @Disabled(value = "Disabled until we learn Mocking") // can be added value!
+class OwnerSDJpaServiceTest {
+
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Disabled
+    @Test
+    void findByLastName() {
+        Owner foundOwner = service.findByLastName("Buck");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
